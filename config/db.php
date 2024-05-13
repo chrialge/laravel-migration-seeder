e0be5913d22aa021193994223d5fef43b7344944
chrialge <chrialge@gmail.com>
2024-05-13 12:33:46 +0200

    add inside config an file that contains trains with name 'db.php' and add at database

---
+++ config/db.php
@@ -0,0 +1,61 @@
+<?php
+
+return [
+    'trains' =>
+    [
+        [
+            'agency' => 'TrenoEuropeo',
+            'starting_station' => 'Milano',
+            'arrival_station' => 'Amsterdam',
+            'starting_time' => '10:00:00',
+            'arrival_time' => '23:00:00',
+            'data' => '2024-05-13',
+            'code_train' => 'EDF3214',
+            'number_binary' => '9',
+            'in_time' => 1,
+            'deleted' => 0,
+
+        ],
+        [
+            'agency' => 'TrenoRegionale',
+            'starting_station' => 'Pesaro',
+            'arrival_station' => 'Fano',
+            'starting_time' => '11:00:00',
+            'arrival_time' => '12:30:00',
+            'data' => '2024-05-12',
+            'code_train' => 'ABC3423',
+            'number_binary' => '4',
+            'in_time' => 1,
+            'deleted' => 0,
+
+        ],
+        [
+            'agency' => 'TrenoRotti',
+            'starting_station' => 'Cattolica',
+            'arrival_station' => 'Cesena',
+            'starting_time' => '15:00:00',
+            'arrival_time' => '17:00:00',
+            'data' => '2024-05-13',
+            'code_train' => 'DEA0019',
+            'number_binary' => '6',
+            'in_time' => 1,
+            'deleted' => 0,
+
+        ],
+        [
+            'agency' => 'TreniItalia',
+            'starting_station' => 'ReggioCalabria',
+            'arrival_station' => 'Napoli',
+            'starting_time' => '08:00:00',
+            'arrival_time' => '13:00:00',
+            'data' => '2024-05-11',
+            'code_train' => 'NAP0003',
+            'number_binary' => '1',
+            'in_time' => 1,
+            'deleted' => 0,
+
+        ],
+    ]
+];
+
+?>
--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -13,21 +13,23 @@ class TrainsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        $trains = [
-            [
-                'agency' => 'TrenoEuropeo',
-                'starting_station' => 'Milano',
-                'arrival_station' => 'Amsterdam',
-                'starting_time' => '10:00:00',
-                'arrival_time' => '23:00:00',
-                'data' => '2024-05-13',
-                'code_train' => 'EDF3214',
-                'number_binary' => '9',
-                'in_time' => 1,
-                'deleted' => 0,
 
-            ]
-        ];
+        $trains = config('db.trains');
+        // $trains = [
+        //     [
+        //         'agency' => 'TrenoEuropeo',
+        //         'starting_station' => 'Milano',
+        //         'arrival_station' => 'Amsterdam',
+        //         'starting_time' => '10:00:00',
+        //         'arrival_time' => '23:00:00',
+        //         'data' => '2024-05-13',
+        //         'code_train' => 'EDF3214',
+        //         'number_binary' => '9',
+        //         'in_time' => 1,
+        //         'deleted' => 0,
+
+        //     ]
+        // ];
 
         foreach ($trains as $train) {
             $newTrain = new Train();
