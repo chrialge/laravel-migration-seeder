e62ff4ef85828ccdf2e3ca7f06b5b223fb6d904c
chrialge <chrialge@gmail.com>
2024-05-13 13:01:24 +0200

    add faker for data fake and add inside table trains

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Database\Seeders;
-
+use Faker\Generator as Faker;
 use App\Models\Train;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -11,7 +11,7 @@ class TrainsTableSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
 
         $trains = config('db.trains');
@@ -31,20 +31,35 @@ class TrainsTableSeeder extends Seeder
         //     ]
         // ];
 
-        foreach ($trains as $train) {
+        for ($i=0; $i < 1; $i++) { 
             $newTrain = new Train();
-            $newTrain->agency = $train['agency'];
-            $newTrain->starting_station = $train['starting_station'];
-            $newTrain->arrival_station = $train['arrival_station'];
-            $newTrain->starting_time = $train['starting_time'];
-            $newTrain->arrival_time = $train['arrival_time'];
-            $newTrain->data = $train['data'];
-            $newTrain->code_train = $train['code_train'];
-            $newTrain->number_binary = $train['number_binary'];
-            $newTrain->in_time = $train['in_time'];
-            $newTrain->deleted = $train['deleted'];
+            $newTrain->agency = 'Treno'. ucfirst($faker->word(10));
+            $newTrain->starting_station = $faker->city();
+            $newTrain->arrival_station = $faker->city();
+            $newTrain->starting_time = $faker->time();
+            $newTrain->arrival_time = $faker->time();
+            $newTrain->data = $faker->dateTimeBetween('-1 week', '+1 week');
+            $newTrain->code_train = strtoupper($faker->bothify('???####'));
+            $newTrain->number_binary = $faker->randomDigitNotNull();
+            $newTrain->in_time = 0;
+            $newTrain->deleted = 0;
             $newTrain->save();
         }
+
+        // foreach ($trains as $train) {
+        //     $newTrain = new Train();
+        //     $newTrain->agency = $train['agency'];
+        //     $newTrain->starting_station = $train['starting_station'];
+        //     $newTrain->arrival_station = $train['arrival_station'];
+        //     $newTrain->starting_time = $train['starting_time'];
+        //     $newTrain->arrival_time = $train['arrival_time'];
+        //     $newTrain->data = $train['data'];
+        //     $newTrain->code_train = $train['code_train'];
+        //     $newTrain->number_binary = $train['number_binary'];
+        //     $newTrain->in_time = $train['in_time'];
+        //     $newTrain->deleted = $train['deleted'];
+        //     $newTrain->save();
+        // }
 
     }
 }
