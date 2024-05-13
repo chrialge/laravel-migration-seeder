a33c9915661d121467b9acdf6221d989d9fda5a2
chrialge <chrialge@gmail.com>
2024-05-13 12:21:48 +0200

    add seeder and add inside table of the dtabase

--- README.md
+++ README.md
@@ -15,3 +15,7 @@ Ogni treno dovrà avere almeno:
     È probabile che siano necessarie altre colonne per far funzionare la tabella nel modo corretto :occhiolino:
     Inserite inizialmente i dati tramite PhpMyAdmin o artisan tinker per chi ne ha capito l'utilizzo.
     Create il modello Model relativo alla migrazione che avete predisposto al fine di mappare la tabelle del db ed un Controller per mostrare nella home page tutti i treni che sono in partenza dalla data odierna.
+
+## instruction 2
+
+usando la stessa repo Aggiungiamo un seeder per la classe Train usando un array di dati fittizzi fatta a mano.
---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Train;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $trains = [
+            [
+                'agency' => 'TrenoEuropeo',
+                'starting_station' => 'Milano',
+                'arrival_station' => 'Amsterdam',
+                'starting_time' => '10:00:00',
+                'arrival_time' => '23:00:00',
+                'data' => '2024-05-13',
+                'code_train' => 'EDF3214',
+                'number_binary' => '9',
+                'in_time' => 1,
+                'deleted' => 0,
+
+            ]
+        ];
+
+        foreach ($trains as $train) {
+            $newTrain = new Train();
+            $newTrain->agency = $train['agency'];
+            $newTrain->starting_station = $train['starting_station'];
+            $newTrain->arrival_station = $train['arrival_station'];
+            $newTrain->starting_time = $train['starting_time'];
+            $newTrain->arrival_time = $train['arrival_time'];
+            $newTrain->data = $train['data'];
+            $newTrain->code_train = $train['code_train'];
+            $newTrain->number_binary = $train['number_binary'];
+            $newTrain->in_time = $train['in_time'];
+            $newTrain->deleted = $train['deleted'];
+            $newTrain->save();
+        }
+
+    }
+}
+
